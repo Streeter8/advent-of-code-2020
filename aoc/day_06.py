291009091226969df91a9cba35b093bf5ab376e6
Streeter8 <jstreeter@campaignmonitor.com>
2019-12-23 16:42:35 -0600

    :sparkles: Add solution for day 6 part 2

--- aoc/day_06.py
+++ aoc/day_06.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 class Orbit:
     def __init__(self, input: str):
         inputs = input.split(")")
@@ -9,8 +10,9 @@ class Orbit:
 
 
 class Planet:
-    def __init__(self, name: str):
+    def __init__(self, name: str, parent_planet):
         self.name = name
+        self.parent_planet = parent_planet
         self.moons = []
 
     def number_of_orbits(self, level: int = 0) -> int:
@@ -19,6 +21,15 @@ class Planet:
     def add_moon(self, moon):
         self.moons.append(moon)
 
+    @property
+    def planet_path(self):
+        if self.parent_planet:
+            parent_path = self.parent_planet.planet_path
+            parent_path.append(self.name)
+            return parent_path
+        else:
+            return [self.name]
+
 
 class AocOrbits:
     """
@@ -26,7 +37,17 @@ class AocOrbits:
     """
     DIRECTORY = os.path.dirname(os.path.abspath(__file__))
     INPUT_FILE = "day_06.txt"
+    INPUT_FILE_PART_TWO = "day_06_part_two.txt"
     orbits = {}
+    center_of_mass = None
+    you_planet = None
+    santa_planet = None
+
+    def reset(self):
+        self.orbits = {}
+        self.center_of_mass = None
+        self.you_planet = None
+        self.santa_planet = None
 
     def part_one(self):
         input_file_path = os.path.join(self.DIRECTORY, "inputs", self.INPUT_FILE)
@@ -39,21 +60,45 @@ class AocOrbits:
                 else:
                     self.orbits[orbit.planet] = [orbit.moon]
 
-        center_of_mass = Planet("COM")
+        center_of_mass = Planet("COM", None)
         self.process_moons(center_of_mass)
-        print(f"The total number of direct and indirect orbits: {center_of_mass.number_of_orbits()}")
-        # Answer: 186597
+        self.center_of_mass = center_of_mass
+
+        print(f"The total number of direct and indirect orbits: {self.center_of_mass.number_of_orbits()}")
+        # Result: 186597
+
+    def part_two(self):
+        input_file_path = os.path.join(self.DIRECTORY, "inputs", self.INPUT_FILE_PART_TWO)
+
+        with open(input_file_path) as input_file:
+            for input_line in input_file:
+                orbit = Orbit(input_line.replace("\n", ""))
+                if orbit.planet in self.orbits:
+                    self.orbits[orbit.planet].append(orbit.moon)
+                else:
+                    self.orbits[orbit.planet] = [orbit.moon]
+
+        center_of_mass = Planet("COM", None)
+        self.process_moons(center_of_mass)
+        self.center_of_mass = center_of_mass
+
+        you_path = self.you_planet.planet_path
+        santa_path = self.santa_planet.planet_path
+        diff = (set(you_path).symmetric_difference(set(santa_path))).difference({"SAN", "YOU"})
+
+        print(f"Minimal number of orbital jumps required: {len(diff)}")
+        # Result: 412
 
     def process_moons(self, planet: Planet):
         if planet.name in self.orbits:
             for moon in self.orbits[planet.name]:
-                new_moon = Planet(moon)
+                new_moon = Planet(name=moon, parent_planet=planet)
                 self.process_moons(new_moon)
                 planet.add_moon(new_moon)
-
-    def part_two(self):
-        total = "Not Implemented"
-        print(f"Part 2 Solution: {total}")
+                if new_moon.name == "YOU":
+                    self.you_planet = new_moon
+                if new_moon.name == "SAN":
+                    self.santa_planet = new_moon
 
 
 def main():
@@ -65,7 +110,8 @@ def main():
     sys.setrecursionlimit(1075)
     aoc = AocOrbits()
     aoc.part_one()
-    # aoc.part_two()
+    aoc.reset()
+    aoc.part_two()
 
 
 if __name__ == "__main__":
