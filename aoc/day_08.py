e56f7f40bfd8ad0a808a2db481b13f72149309ba
Streeter8 <jstreeter@campaignmonitor.com>
2019-12-23 17:55:58 -0600

    :moneybag: Add day 8 part 2 solution

--- aoc/day_08.py
+++ aoc/day_08.py
@@ -3,7 +3,7 @@ import os
 
 class Row:
     def __init__(self, row_input: str):
-        self.input = row_input
+        self.input = [character for character in row_input]
         characters = {character for character in row_input}
         self.characters = {
             character: row_input.count(character)
@@ -12,7 +12,7 @@ class Row:
 
 
 class Layer:
-    def __init__(self, width, height, layer_input):
+    def __init__(self, width, layer_input):
         self.rows = []
         self.characters = {}
         for index in range(0, len(layer_input), width):
@@ -23,6 +23,9 @@ class Layer:
                     self.characters[character] = row.characters[character]
                 else:
                     self.characters[character] = self.characters[character] + row.characters[character]
+
+    def get_pixel(self, x: int, y: int) -> str:
+        return self.rows[y].input[x]
 
     def _chars(self, char: str) -> int:
         return self.characters.get(char, 0)
@@ -40,6 +43,34 @@ class Layer:
         return self._chars("2")
 
 
+class SecretImage:
+    def __init__(self, width, height, file_input):
+        self.width = width
+        self.height = height
+        layer_length = width * height
+        self.layers = [
+            Layer(width, file_input[index: index + layer_length])
+            for index in range(0, len(file_input), layer_length)
+        ]
+
+    def translate_image(self) -> str:
+        image_string = ""
+        for y_index in range(0, self.height):
+            for x_index in range(0, self.width):
+                image_string = f"{image_string}{self.get_pixel(x_index, y_index)}"
+            image_string = f"{image_string}\n"
+        return image_string
+
+    def get_pixel(self, x: int, y: int) -> str:
+        for index in range(0, len(self.layers)):
+            pixel = self.layers[index].get_pixel(x, y)
+            if pixel == "1":
+                return "X"
+            if pixel == "0":
+                return " "
+        return " "
+
+
 class Aoc:
     """
     Template file for solutions
@@ -49,7 +80,7 @@ class Aoc:
     WIDTH = 25
     HEIGHT = 6
 
-    def part_one(self):
+    def run(self):
         input_file_path = os.path.join(self.DIRECTORY, "inputs", self.INPUT_FILE)
         file_input = None
 
@@ -57,14 +88,10 @@ class Aoc:
             for input_line in input_file:
                 file_input = input_line.replace("\n", "")
 
-        layer_length = self.WIDTH * self.HEIGHT
-        layers = [
-            Layer(self.WIDTH, self.HEIGHT, file_input[index: index + layer_length])
-            for index in range(0, len(file_input), layer_length)
-        ]
+        secret_image = SecretImage(self.WIDTH, self.HEIGHT, file_input)
 
         lowest_layer = None
-        for layer in layers:
+        for layer in secret_image.layers:
             if lowest_layer:
                 if lowest_layer.zeroes > layer.zeroes:
                     lowest_layer = layer
@@ -74,15 +101,14 @@ class Aoc:
         print(f"Part 1 Solution: {lowest_layer.ones * lowest_layer.twos}")
         # Result: 2048
 
-    def part_two(self):
-        total = "Not Implemented"
-        print(f"Part 2 Solution: {total}")
+        result = secret_image.translate_image()
+        print(f"Part 2 Solution:\n{result}")
+        # Result: HFYAK
 
 
 def main():
     aoc = Aoc()
-    aoc.part_one()
-    aoc.part_two()
+    aoc.run()
 
 
 if __name__ == "__main__":
