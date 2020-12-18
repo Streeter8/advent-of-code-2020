8508ae812aa6cbe4d3542493d18621bd331710e4
Streeter8 <jstreeter@campaignmonitor.com>
2020-12-18 15:23:06 -0600

    :white_check_mark: Valid solution for day 9

--- aoc/2020/day_09.py
+++ aoc/2020/day_09.py
@@ -1,5 +1,6 @@
 import os
 from timeit import timeit
+from typing import List
 
 
 class AocNine:
@@ -7,7 +8,7 @@ class AocNine:
     INPUT_FILE = "day_09.txt"
 
     def __init__(self):
-        self.allowed_numbers = set()
+        self.invalid_number = None
 
     def line_generator(self):
         input_file_path = os.path.join(self.DIRECTORY, "inputs", self.INPUT_FILE)
@@ -16,7 +17,7 @@ class AocNine:
                 yield input_line.replace("\n", "")
 
     @property
-    def _input(self) -> list:
+    def _input(self) -> List[int]:
         # return [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
         return [int(number) for number in self.line_generator()]
 
@@ -25,32 +26,46 @@ class AocNine:
         # return 5
         return 25
 
-    def part_one(self):
-        """~4ms"""
-        # preamble = 5
-        # self.set_allowed_sums(preamble, _input)
-        first = self.find_first_invalid_number(self.preamble, self._input)
-        print(f"The first invalid number is: {first}")
+    def nine(self):
+        """~20ms"""
+        self.set_invalid_number(self.preamble, self._input)
+        weak = self.find_encryption_weakness(self._input)
+        print(f"Encryption weakness is: {weak}")
 
-    def is_allowed_number(self, number: int, numbers: list):
+    def is_allowed_number(self, number: int, numbers: list) -> bool:
         len_numbers = len(numbers)
         for index_one in range(len_numbers):
-            for index_two in range(index_one, len_numbers):
+            for index_two in range(index_one + 1, len_numbers):
                 if number == numbers[index_one] + numbers[index_two]:
                     return True
 
         return False
 
-    def find_first_invalid_number(self, preamble: int, _input: list) -> int:
+    def set_invalid_number(self, preamble: int, _input: list) -> None:
         for index in range(preamble, len(_input)):
-            if not self.is_allowed_number(_input[index], _input[index-preamble:index]):
-                return _input[index]
-        raise Exception("No invalid numbers found")
+            if not self.is_allowed_number(_input[index], _input[index - preamble:index]):
+                print(f"The first invalid number is: {_input[index]}")
+                self.invalid_number = _input[index]
+                break
+
+    def find_encryption_weakness(self, _input: list) -> int:
+        if not self.invalid_number:
+            raise Exception("Invalid number not set")
+
+        len_numbers = len(_input)
+        for index_one in range(len_numbers):
+            running_sum = _input[index_one]
+            for index_two in range(index_one + 1, len_numbers):
+                running_sum += _input[index_two]
+                if running_sum == self.invalid_number:
+                    return min(_input[index_one:index_two + 1]) + max(_input[index_one:index_two + 1])
+                if running_sum > self.invalid_number:
+                    break
 
 
 def main():
     aoc = AocNine()
-    print(f"Part one took {timeit(aoc.part_one, number=1)} seconds to execute")
+    print(f"Problem 9 took {timeit(aoc.nine, number=1)} seconds to execute")
 
 
 if __name__ == "__main__":
