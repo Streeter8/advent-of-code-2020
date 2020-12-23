c75db315c2f1b183bbdb2f1adc3bde4e08c99a60
Streeter8 <jstreeter@campaignmonitor.com>
2020-12-23 17:16:07 -0600

    :construction: More improvements
    Getting better, but I think the index lookup is the bottleneck. This will require a significant refactor. So saving state to keep iterating

--- aoc/2020/day_23.py
+++ aoc/2020/day_23.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from timeit import timeit
 from typing import List
 
@@ -9,8 +10,13 @@ PART_TWO_MOVES = 10000000
 
 
 class AocTwentyThree:
-    def __init__(self):
-        self.cups = CUPS_SAMPLE
+    def __init__(self, test_input: bool = True):
+        if test_input:
+            self.cups = CUPS_SAMPLE
+            self.part_one_solution = "67384529"
+        else:
+            self.cups = PUZZLE_INPUT
+            self.part_one_solution = "69852437"
 
     def part_one(self):
         """0.1ms"""
@@ -22,15 +28,25 @@ class AocTwentyThree:
         cups_after_one = self.apply_one_shift(cups)
 
         solution = "".join(str(cup) for cup in cups_after_one)
+        assert self.part_one_solution == solution
         print(f"Part one solution: {solution}")
 
     def part_two(self):
         cups = [int(cup) for cup in self.cups]
-        cups = cups + [cup for cup in range(max(cups), PART_TWO_CUPS + 1)]
+        cups.extend([cup for cup in range(max(cups) + 1, PART_TWO_CUPS + 1)])
         number_of_cups = len(cups)
+        beginning = self.now
         for _turn in range(PART_TWO_MOVES):
-            if _turn % 100 == 0:
+            if _turn % 100 == 0 and _turn != 0:
+                delta = self.now - beginning
+                total_time = delta.seconds + (delta.microseconds / 1000000)
+                average_time = total_time / _turn
+
                 print(f"Executing turn {_turn}: Turns remaining: {PART_TWO_MOVES - _turn}")
+                print(f"Total time to execute {_turn} turns: {total_time} seconds")
+                print(f"Average time: {average_time} seconds per turn")
+                print(f"\n==================================================================\n")
+
             cups = self.move(cups, number_of_cups)
 
         cups_after_one = self.apply_one_shift(cups)
@@ -39,28 +55,50 @@ class AocTwentyThree:
         print(f"Part two solution: {solution}")
 
     def move(self, cups: List[int], number_of_cups: int) -> List[int]:
-        current_cup = cups[0]
-        pick_up = cups[1:4]
-        remaining_cups = cups[:1] + cups[4:]
-        destination = self.determine_destination(current_cup, remaining_cups, number_of_cups)
-        index = remaining_cups.index(destination)
-        new_cups = remaining_cups[0: index + 1] + pick_up + remaining_cups[index + 1:len(remaining_cups)]
+        # current_cup = cups[0]
+        # pick_up = cups[1:4]
+        pick_up = [cups.pop(1), cups.pop(1), cups.pop(1)]
+
+        # remaining_cups = cups[:1] + cups[4:]
+        # del cups[3]
+        # del cups[2]
+        # del cups[1]
+
+        destination = self.determine_destination(cups[0], pick_up, number_of_cups + 1)
+        index = cups.index(destination)
+
+        # new_cups = cups[0: index + 1] + pick_up + cups[index + 1:number_of_cups - 3]
+        cups.insert(index + 1, pick_up[2])
+        cups.insert(index + 1, pick_up[1])
+        cups.insert(index + 1, pick_up[0])
 
         # moving first cup to the end 'shifts' the current cup for the next turn
-        result = new_cups[1:] + new_cups[:1]
-        return result
+        # result = new_cups[1:] + new_cups[:1]
+        cups.append(cups.pop(0))
+        return cups
 
-    def determine_destination(self, current_cup: int, remaining_cups: List[int], number_of_cups: int) -> int:
-        destination = (current_cup - 1) % (number_of_cups + 1)
-        while destination not in remaining_cups:
-            destination = (destination - 1) % (number_of_cups + 1)
+    def determine_destination(self, current_cup: int, picked_up_cups: List[int], cap: int) -> int:
+        destination = self._determine_destination(current_cup, cap)
+
+        while destination in picked_up_cups:
+            destination = self._determine_destination(destination, cap)
 
         return destination
+
+    def _determine_destination(self, destination: int, cap: int) -> int:
+        if (_destination := (destination - 1) % cap) != 0:
+            return _destination
+
+        return cap - 1
 
     def apply_one_shift(self, cups):
         one_index = cups.index(1)
         cups_after_one = cups[one_index + 1:len(cups)] + cups[0:one_index]
         return cups_after_one
+
+    @property
+    def now(self) -> datetime:
+        return datetime.now(timezone.utc)
 
 
 def main():
