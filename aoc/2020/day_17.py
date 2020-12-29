7434a7af729e7e90b7cfc053c43712bc40aa1920
Streeter8 <jstreeter@campaignmonitor.com>
2020-12-29 10:33:29 -0600

    :white_check_mark: Valid day 17 solution

--- aoc/2020/day_17.py
+++ aoc/2020/day_17.py
@@ -37,6 +37,16 @@ class Cube:
         return 1 if self._active else 0
 
 
+class Hypercube(Cube):
+    def __init__(self, x: int, y: int, z: int, line_input: str):
+        super().__init__(x, y, z, line_input)
+        self.w = 0
+
+    @property
+    def point(self) -> str:
+        return f"{self.x},{self.y},{self.z},{self.w}"
+
+
 class Cubes:
     def __init__(self, cubes: Dict[str, int]):
         self.cubes = cubes
@@ -82,7 +92,11 @@ class Cubes:
 
                 cubes_assessed.add(adjacent_cube)
 
-        return Cubes(cubes_to_pass)
+        return self.new_cube(cubes_to_pass)
+
+    @classmethod
+    def new_cube(cls, cubes: Dict[str, int]):
+        return cls(cubes)
 
     def flip_cube(self, point: str) -> bool:
         cube_is_active = self.cubes.get(point, 0)
@@ -93,61 +107,69 @@ class Cubes:
 
     def flip_active_cube(self, point: str) -> bool:
         number_of_adjacent_active_cubes = 0
-        for adjacent_hexagon in self.adjacent_cubes(point, include_self=False):
-            number_of_adjacent_active_cubes += self.cubes.get(adjacent_hexagon, 0)
+        for adjacent_cube in self.adjacent_cubes(point):
+            if adjacent_cube == point:
+                # exclude self in evaluation
+                continue
+
+            number_of_adjacent_active_cubes += self.cubes.get(adjacent_cube, 0)
             if number_of_adjacent_active_cubes > 3:
+                # Deactivate cube when more than 3 neighbors are active
                 return True
 
-        return not (2 <= number_of_adjacent_active_cubes <= 3)
+        # Do not deactivate cube when exactly 2 or 3 neighbors are active
+        return not (
+            number_of_adjacent_active_cubes == 2
+            or number_of_adjacent_active_cubes == 3
+        )
 
     def flip_inactive_cube(self, point: str) -> bool:
         number_of_adjacent_active_cubes = 0
-        for adjacent_hexagon in self.adjacent_cubes(point, include_self=False):
-            number_of_adjacent_active_cubes += self.cubes.get(adjacent_hexagon, 0)
+        for adjacent_cube in self.adjacent_cubes(point):
+            if adjacent_cube == point:
+                # exclude self in evaluation
+                continue
+
+            number_of_adjacent_active_cubes += self.cubes.get(adjacent_cube, 0)
             if number_of_adjacent_active_cubes > 3:
+                # Do not activate cube when more than 3 neighbors are active
                 return False
 
+        # Activate inactive cube when exactly 3 neighbors are active
         return number_of_adjacent_active_cubes == 3
 
-    def adjacent_cubes(self, point: str, include_self: bool = True):
+    def adjacent_cubes(self, point: str):
         x, y, z = (int(coordinate) for coordinate in point.split(","))
+        for cube in self._adjacent_cubes(x, y, z):
+            yield cube
 
-        yield f"{x - 1},{y - 1},{z - 1}"
-        yield f"{x - 1},{y - 1},{z}"
-        yield f"{x - 1},{y - 1},{z + 1}"
+    def _adjacent_cubes(self, x: int, y: int, z: int):
+        for _x in range(x - 1, x + 2):
+            for _y in range(y - 1, y + 2):
+                for _z in range(z - 1, z + 2):
+                    yield f"{_x},{_y},{_z}"
 
-        yield f"{x - 1},{y},{z - 1}"
-        yield f"{x - 1},{y},{z}"
-        yield f"{x - 1},{y},{z + 1}"
 
-        yield f"{x - 1},{y + 1},{z - 1}"
-        yield f"{x - 1},{y + 1},{z}"
-        yield f"{x - 1},{y + 1},{z + 1}"
+class Hypercubes(Cubes):
+    @classmethod
+    def from_line_generator(cls, line_generator):
+        hypercubes = {}
 
-        yield f"{x},{y - 1},{z - 1}"
-        yield f"{x},{y - 1},{z}"
-        yield f"{x},{y - 1},{z + 1}"
+        line_number = 0
+        for line in line_generator:
+            for index in range(len(line)):
+                cube = Hypercube(x=index, y=line_number, z=0, line_input=line[index])
+                if cube.active:
+                    hypercubes[cube.point] = cube.active
+            line_number += 1
 
-        yield f"{x},{y},{z - 1}"
-        if include_self:
-            yield f"{x},{y},{z}"  # self
-        yield f"{x},{y},{z + 1}"
+        return cls(hypercubes)
 
-        yield f"{x},{y + 1},{z - 1}"
-        yield f"{x},{y + 1},{z}"
-        yield f"{x},{y + 1},{z + 1}"
-
-        yield f"{x + 1},{y - 1},{z - 1}"
-        yield f"{x + 1},{y - 1},{z}"
-        yield f"{x + 1},{y - 1},{z + 1}"
-
-        yield f"{x + 1},{y},{z - 1}"
-        yield f"{x + 1},{y},{z}"
-        yield f"{x + 1},{y},{z + 1}"
-
-        yield f"{x + 1},{y + 1},{z - 1}"
-        yield f"{x + 1},{y + 1},{z}"
-        yield f"{x + 1},{y + 1},{z + 1}"
+    def adjacent_cubes(self, point: str, include_self: bool = True):
+        x, y, z, w = (int(coordinate) for coordinate in point.split(","))
+        for _w in range(w - 1, w + 2):
+            for cube in self._adjacent_cubes(x, y, z):
+                yield f"{cube},{_w}"
 
 
 class AocSeventeen:
@@ -156,10 +178,10 @@ class AocSeventeen:
         self.test_case = test_case
         if test_case:
             self.part_one_expected_result = 112
-            self.part_two_expected_results = None
+            self.part_two_expected_result = 848
         else:
-            self.part_one_expected_result = None
-            self.part_two_expected_results = None
+            self.part_one_expected_result = 242
+            self.part_two_expected_result = None
 
     def line_generator(self):
         if self.test_case:
@@ -172,18 +194,31 @@ class AocSeventeen:
                 yield input_line
 
     def part_one(self):
-        """~75ms"""
+        """~100ms"""
         cubes = Cubes.from_line_generator(self.line_generator())
-        for days in range(6):
+        for day in range(6):
             cubes = cubes.next_day
 
-        # assert self.part_one_expected_result == cubes.active_cubes
+        if self.part_one_expected_result:
+            assert self.part_one_expected_result == cubes.active_cubes
         print(f"Part 1 solution: {cubes.active_cubes}")
+
+    def part_two(self):
+        """~2.75s"""
+        hypercubes = Hypercubes.from_line_generator(self.line_generator())
+        for day in range(6):
+            hypercubes = hypercubes.next_day
+
+        if self.part_two_expected_result:
+            assert self.part_two_expected_result == hypercubes.active_cubes
+
+        print(f"Part 2 solution: {hypercubes.active_cubes}")
 
 
 def main():
     aoc = AocSeventeen(test_case=False)
-    print(f"Problem 17 took {timeit(aoc.part_one, number=1)} seconds to execute")
+    print(f"Part 1 took {timeit(aoc.part_one, number=1)} seconds to execute")
+    print(f"Part 2 took {timeit(aoc.part_two, number=1)} seconds to execute")
 
 
 if __name__ == "__main__":
