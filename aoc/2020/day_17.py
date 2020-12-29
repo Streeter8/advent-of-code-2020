a0fca03f3ca1f6414e42ddd1672c1b4feca9e1d8
Streeter8 <jstreeter@campaignmonitor.com>
2020-12-29 10:37:54 -0600

    :art: Reduce code redundancy

--- aoc/2020/day_17.py
+++ aoc/2020/day_17.py
@@ -19,34 +19,6 @@ PUZZLE_INPUT = [
 ]
 
 
-class Cube:
-    def __init__(self, x: int, y: int, z: int, line_input: str):
-        self.line_input = line_input
-        self._active = "#" == line_input
-
-        self.x = x
-        self.y = y
-        self.z = z
-
-    @property
-    def point(self) -> str:
-        return f"{self.x},{self.y},{self.z}"
-
-    @property
-    def active(self) -> int:
-        return 1 if self._active else 0
-
-
-class Hypercube(Cube):
-    def __init__(self, x: int, y: int, z: int, line_input: str):
-        super().__init__(x, y, z, line_input)
-        self.w = 0
-
-    @property
-    def point(self) -> str:
-        return f"{self.x},{self.y},{self.z},{self.w}"
-
-
 class Cubes:
     def __init__(self, cubes: Dict[str, int]):
         self.cubes = cubes
@@ -58,11 +30,18 @@ class Cubes:
         line_number = 0
         for line in line_generator:
             for index in range(len(line)):
-                cube = Cube(x=index, y=line_number, z=0, line_input=line[index])
-                if cube.active:
-                    cubes[cube.point] = cube.active
+                if "#" == line[index]:
+                    cubes[cls._initial_point(index, line_number)] = 1
             line_number += 1
 
+        return cls(cubes)
+
+    @staticmethod
+    def _initial_point(x: int, y: int) -> str:
+        return f"{x},{y},0"
+
+    @classmethod
+    def new_cube(cls, cubes: Dict[str, int]):
         return cls(cubes)
 
     @property
@@ -93,10 +72,6 @@ class Cubes:
                 cubes_assessed.add(adjacent_cube)
 
         return self.new_cube(cubes_to_pass)
-
-    @classmethod
-    def new_cube(cls, cubes: Dict[str, int]):
-        return cls(cubes)
 
     def flip_cube(self, point: str) -> bool:
         cube_is_active = self.cubes.get(point, 0)
@@ -151,19 +126,9 @@ class Cubes:
 
 
 class Hypercubes(Cubes):
-    @classmethod
-    def from_line_generator(cls, line_generator):
-        hypercubes = {}
-
-        line_number = 0
-        for line in line_generator:
-            for index in range(len(line)):
-                cube = Hypercube(x=index, y=line_number, z=0, line_input=line[index])
-                if cube.active:
-                    hypercubes[cube.point] = cube.active
-            line_number += 1
-
-        return cls(hypercubes)
+    @staticmethod
+    def _initial_point(x: int, y: int) -> str:
+        return f"{x},{y},0,0"
 
     def adjacent_cubes(self, point: str, include_self: bool = True):
         x, y, z, w = (int(coordinate) for coordinate in point.split(","))
